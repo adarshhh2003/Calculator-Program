52ef9c108988f4b9ffe60c5f87bfada819aaec62
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-01-06 23:49:46 +0530

    Addition functionality added

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,33 @@ public class Calculator{
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
 
- 			System.out.println("\nWelcome to the Calculator");
+		while(true){
+			System.out.println("\nWelcome to the Calculator");
+			System.out.println("Enter '+' for Addition");
+			System.out.println("Enter 'x' to Exit");
+			System.out.println("Choose an operation: ");
+			
+			char choice = scanner.next().charAt(0);
+			
+			if(choice == 'x' || choice == 'X'){
+				System.out.println("Exiting the Calculator!!");
+				break;
+			}
+
+			System.out.println("Enter the first number: ");
+			double num1 = scanner.nextDouble();
+
+			System.out.println("Enter the second number");
+			double num2 = scanner.nextDouble();
+
+			switch(choice){
+				case '+':
+					System.out.println("Result: " + (num1 + num2));
+					break;
+				default:
+					System.out.println("Invalid choice. Please choice a valid operation.");
+			}
+		}
 		
 		scanner.close();
 	}
